a5b4c09e415b9bcab0a1a877ed0d1d011f894434
Badoulahi8 <badoulahi8@gmail.com>
2023-06-26 10:29:07 +0000

    Init Groupie Tracker

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+const (
+	PORT = ":8080"
+	path = "view/"
+)
+
+func main() {
+
+	http.HandleFunc("/", WelcomePage)
+	http.HandleFunc("/artists", Artists)
+	http.HandleFunc("/locations", Locations)
+
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+	fmt.Println("(http://localhost:8080) Server started on port:", PORT)
+	http.ListenAndServe(PORT, nil)
+}
+
+func WelcomePage(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		fmt.Fprintf(w, "Page error")
+		return
+	}
+
+	renderTemplate(w, "index", nil)
+}
+
+func Artists(w http.ResponseWriter, r *http.Request) {
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintln(w, string(responseData))
+}
+
+func Locations(w http.ResponseWriter, r *http.Request) {
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api/locations")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintln(w, string(responseData))
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+	t, err := template.ParseFiles(path + tmpl + ".html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
