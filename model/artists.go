5be4d0c1c905e2ba765ea7b553d9bcbe68273e5b
alsow <badoulahi8@gmail.com>
2023-07-02 22:49:51 +0000

    Update

--- model/artists.go
+++ model/artists.go
@@ -6,8 +6,10 @@ import (
 )
 
 type Artist struct {
-	Id   int    `json:"id"`
-	Name string `json:"name"`
+	Id           int         `json:"id"`
+	Name         string      `json:"name"`
+	Image        string      `json:"image"`
+	CreationDate interface{} `json:"creationDate"`
 }
 
 const (
